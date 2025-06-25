82fe4b53b0d084a1dc7e7538cf534845054f3e89
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 00:45:53 +0530

    Updated code files via regeneration: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,38 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Homepage from './pages/Homepage';
+import VideoPlaybackPage from './pages/VideoPlaybackPage';
+import ContentListingPage from './pages/ContentListingPage';
+import UserProfilePage from './pages/UserProfilePage';
+import AuthPage from './pages/AuthPage';
 
-
-import AuthPage from "./pages/AuthPage";
-import ContentListingPage from "./pages/ContentListingPage";
-import Homepage from "./pages/Homepage";
-import UserProfilePage from "./pages/UserProfilePage";
-import VideoPlaybackPage from "./pages/VideoPlaybackPage";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-
-const App = () => (
-<QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
-
-
-          <Route path="/" element={<Homepage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/content-listing" element={<ContentListingPage />} />
-          <Route path="/user-profile" element={<UserProfilePage />} />
-          <Route path="/video-playback" element={<VideoPlaybackPage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-    </BrowserRouter>
-    </TooltipProvider>
-</QueryClientProvider>
+const AppLayout = () => (
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-grow">
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
 );
+
+function App() {
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/watch/:id" element={<VideoPlaybackPage />} />
+            <Route path="/browse" element={<ContentListingPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+          </Route>
+          <Route path="/login" element={<AuthPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
